0b1634cf7a453ed4fe32e8a578878b9a522c902c
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-03 20:00:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const getNames = function(driverObj) {
+    console.log(driverObj.name)
+}
+
+function logDriverNames(drivers) {
+    return drivers.forEach(getNames)
+}
+
+function logDriversByHometown(drivers, ht) {
+    return drivers.forEach(function(driverObj) {
+        if (driverObj.hometown === ht) {
+            console.log(driverObj.name)
+        }
+    })
+}
+
+function driversByRevenue(drivers) {
+    let nuDrivers = [...drivers]
+    return nuDrivers.sort(function(a, b) {
+        return a.revenue > b.revenue
+    })
+}
+
+function driversByName(drivers) {
+    let nuDrivers = [...drivers]
+    return nuDrivers.sort(function(a, b) {
+        return a.name.localeCompare(b.name)
+    })
+}
+
+  function totalRevenue(drivers) {
+    return drivers.reduce(function(agg, el) {
+        return agg + el.revenue
+    }, 0)
+}
+
+  function averageRevenue(drivers) {
+    const a = drivers.reduce(function(agg, el, i, arr) {
+        return agg + el.revenue
+        
+    }, 0)
+    return a/drivers.length
+}
